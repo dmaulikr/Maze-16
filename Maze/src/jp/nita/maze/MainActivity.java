87ff3275ccc95ef5bbb524e4ba8bdc2c050e75d3
kenta <kent.ruffle.mgj626@gmail.com>
2014-10-30 18:17:48 +0900

    first commit

---
+++ Maze/src/jp/nita/maze/MainActivity.java
@@ -0,0 +1,32 @@
+package jp.nita.maze;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	private GLSurfaceView mGLSurfaceView;
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mGLSurfaceView = new GLSurfaceView(this);
+		mGLSurfaceView.setEGLContextClientVersion(2);
+		mGLSurfaceView.setRenderer(new MazeRenderer());
+		setContentView(mGLSurfaceView);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mGLSurfaceView.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		mGLSurfaceView.onPause();
+	}
+}
